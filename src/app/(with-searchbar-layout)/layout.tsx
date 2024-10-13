594357f8d7c91e5fc9d8cb47caa5f088f3abd056
yongin <rladls6412@naver.com>
2024-10-13 19:48:37 +0900

    Day10 미션

--- src/app/(with-searchbar-layout)/layout.tsx
+++ src/app/(with-searchbar-layout)/layout.tsx
@@ -1,9 +1,10 @@
+import SearchBar from "@/components/searchbar";
 import { ReactNode } from "react";
 
 export default function SearchLayout({ children }: { children: ReactNode }) {
   return (
     <div>
-      <div>Searchbar Layout</div>
+      <SearchBar />
       {children}
     </div>
   );
---
+++ src/components/searchbar.tsx
@@ -0,0 +1,39 @@
+"use client";
+import { useRouter, useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
+
+export default function SearchBar() {
+  const router = useRouter();
+  const searchParmas = useSearchParams();
+  const q = searchParmas.get("q");
+
+  const [search, setSearch] = useState("");
+
+  const onChangeSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+  };
+
+  const onClickSearchMove = () => {
+    if (search === q || !search) return;
+    router.push(`/search?q=${search}`);
+  };
+
+  const onKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      onClickSearchMove();
+    }
+  };
+
+  useEffect(() => {
+    if (q) {
+      setSearch(q);
+    }
+  }, [q]);
+
+  return (
+    <div>
+      <input onKeyDown={onKeyDown} value={search} onChange={onChangeSearch} />
+      <button onClick={onClickSearchMove}>검색</button>
+    </div>
+  );
+}
